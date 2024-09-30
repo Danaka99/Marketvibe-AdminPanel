1eb7ea053014af543fb81bc9a504e4f8046d5914
Danaka99 <140838401+Danaka99@users.noreply.github.com>
2024-10-01 01:22:37 +0530

    modify

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
-import marketvibe from '../../assests/Market.png'
+import marketvibe from '../../assests/Market3.png'
 import { MyContext } from '../../App';
-import background from '../../assests/istockphoto-1344849344-612x612.jpg';
+import background from '../../assests/background.avif';
 import { IoMdMail } from "react-icons/io";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { IoEye } from "react-icons/io5";
@@ -29,13 +29,17 @@ const Login = () => {
      <img src={background} alt='' className='loginPattern'/>
      <section className="loginSection">
         <div className="loginBox">
-            <div className='logo text-center'>
+            {/* <div className='logo text-center'>
                 <img src={marketvibe} alt='' width="60px"/>
                 <h5 className='font-weight-bold'>Login to MarketV</h5>
-            </div>
+            </div> */}
 
             <div className='wrapper mt-4 card border'>
                 <form>
+                    <div className='logo text-center mb-3'>
+                        <img src={marketvibe} alt='' width="100px" className='mb-2'/>
+                        <h5 className='font-weight-bold'>Login to MarketV</h5>
+                    </div>
                     <div className= {`form-group mb-4 position-relative ${inputIndex===0 && 'focus'}`}>
                         <span className='icon'><IoMdMail/></span>
                         <input type='text' className='form-control' placeholder='&nbsp;enter your email'
